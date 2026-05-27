e1ed32593a7cd2ccc8823cf3e41aa5c821f901da
Nathan Yee <yee.nathan.c@gmail.com>
2026-05-27 16:38:06 -0700

    version bump

--- modules/metadata_form/versions.tf
+++ modules/metadata_form/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6.37.0"
+      version = ">= 6.46.0"
     }
     awscc = {
       source  = "hashicorp/awscc"
-      version = ">= 1.76.0"
+      version = ">= 1.85.0"
     }
   }
 }
