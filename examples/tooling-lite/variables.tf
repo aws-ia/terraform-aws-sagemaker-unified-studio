129e7624b0e52d78a3b81534360466661a11d9a7
Nathan Yee <yee.nathan.c@gmail.com>
2026-06-24 09:38:18 -0700

    Update region availability

--- examples/tooling-lite/variables.tf
+++ examples/tooling-lite/variables.tf
@@ -12,9 +12,21 @@ variable "aws_region" {
 
   validation {
     condition = contains([
-      "us-east-1", "us-east-2", "us-west-1", "us-west-2",
-      "eu-west-1", "eu-west-2", "eu-central-1",
-      "ap-southeast-1", "ap-southeast-2", "ap-northeast-1"
+      "us-east-1",      # US East (N. Virginia)
+      "us-east-2",      # US East (Ohio)
+      "us-west-2",      # US West (Oregon)
+      "ca-central-1",   # Canada (Central)
+      "sa-east-1",      # South America (São Paulo)
+      "eu-west-1",      # Europe (Ireland)
+      "eu-west-2",      # Europe (London)
+      "eu-west-3",      # Europe (Paris)
+      "eu-central-1",   # Europe (Frankfurt)
+      "eu-north-1",     # Europe (Stockholm)
+      "ap-northeast-1", # Asia Pacific (Tokyo)
+      "ap-northeast-2", # Asia Pacific (Seoul)
+      "ap-southeast-1", # Asia Pacific (Singapore)
+      "ap-southeast-2", # Asia Pacific (Sydney)
+      "ap-south-1"      # Asia Pacific (Mumbai)
     ], var.aws_region)
     error_message = "AWS region must be one of the supported regions for SageMaker Unified Studio."
   }
